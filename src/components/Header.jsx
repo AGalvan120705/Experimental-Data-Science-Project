68415720495d16e43bcdd366d8d0eefcbd462b75
AGalvan120705 <galvan.an@northeastern.edu>
2026-03-21 15:20:47 +0000

    updated header.jsx
    The header was updated to start setting up the side menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import {Menu} from 'lucide-react'
+
 const Header = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white shadow z-50 p-4">
+    <nav className="fixed top-0 left-0 w-full bg-white shadow z-50 p-4 flex items-center justify-start gap-3">
+      <Menu className="w-6 h-6 text-gray-800" />
       <p className="font-bold text-black text-xl">SugarAware</p>
     </nav>
   );
